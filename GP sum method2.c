01aa5d23bf465e294247449b641cf60647e36fd4
ashmeekkaurs <84696860+ashmeekkaurs@users.noreply.github.com>
2021-05-31 21:33:40 +0530

    GP sum

---
+++ GP sum method2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a;//first term
+	int r;//difference
+	int n;//place of last term
+	scanf("%d%d%d",&a,&r,&n);
+	float sum=0;
+	if(r>1)
+	sum=a*(pow(r,n)-1)/(r-1);
+	else
+	sum=a*(1-pow(r,n))/(1-r);
+	printf("%f\n",sum );
+
+    return 0;
+
+}
