7e5a203504e043b45625865d274613c59c6bece1
ashmeekkaurs <84696860+ashmeekkaurs@users.noreply.github.com>
2021-07-18 21:14:36 +0530

    labprogram07

--- main.c
+++ main.c
@@ -1,27 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
-int fibonacci(int); // function declaration
+
 int main()
 {
-    int n;
-    printf("\n enter the number of elements you wish to print: ");
-    scanf("%d", &n);
-    printf("\n0 1");
-    fibonacci(n - 2); //function calling
+	FILE *fptr1, *fptr2;
+	char filename[100], c;
+
+	printf("Enter the filename to open for reading \n");
+	scanf("%s", filename);
+
+	// Open one file for reading
+	fptr1 = fopen(filename, "r");
+	if (fptr1 == NULL)
+	{
+		printf("Cannot open file %s \n", filename);
+		exit(0);
+	}
+
+	printf("Enter the filename to open for writing \n");
+	scanf("%s", filename);
+
+	// Open another file for writing
+	fptr2 = fopen(filename, "w");
+	if (fptr2 == NULL)
+	{
+		printf("Cannot open file %s \n", filename);
+		exit(0);
+	}
+
+	// Read contents from file
+	c = fgetc(fptr1);
+	while (c != EOF)
+	{
+		fputc(c, fptr2);
+		c = fgetc(fptr1);
+	}
+
+	printf("\nContents copied to %s", filename);
+
+	fclose(fptr1);
+	fclose(fptr2);
+	return 0;
 }
-int fibonacci(int n) //function defining
-{
-    static int el1 = 0, el2 = 1; //el stands for element
-    static int new_el;           //the next element
-    if (n > 0)
-    {
-        new_el = el1 + el2;
-        el1 = el2;    //swapping places of elements to get the last element on the place of the precedding one
-        el2 = new_el; //same as above
-        printf(" %d", new_el);
-        fibonacci(n - 1);
-    }
-}
-
-
-
